3ca85ad58662918a542792ce2ecfdc53be1bd144
Marley Kanui <marley.kanui@gmail.com>
2019-10-29 01:04:27 +0000

    J

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,10 +3,14 @@ class Comment < ApplicationRecord
   belongs_to :place
 
    RATINGS = {
-    'one star': '1_star',
-    'two stars': '2_stars',
-    'three stars': '3_stars',
-    'four stars': '4_stars',
-    'five stars': '5_stars'
+    'One Star': '1_star',
+    'Two Stars': '2_stars',
+    'Three Stars': '3_stars',
+    'Four Stars': '4_stars',
+    'Five stars': '5_stars'
   }
+
+  def humanized_rating
+    RATINGS.invert[self.rating]
+  end
 end
